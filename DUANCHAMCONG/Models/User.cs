0b63500e3b2a03d21c234e4db66fc221152d7a98
minhg <minhgaafk@gmail.com>
2026-05-11 07:40:34 +0700

    feat: add User model and configure static web assets for project deployment

--- DUANCHAMCONG/Models/User.cs
+++ DUANCHAMCONG/Models/User.cs
@@ -4,13 +4,13 @@ namespace AttendanceSystem.Models
     {
         public int Id { get; set; }
 
-        public string FullName { get; set; }
+        public required string FullName { get; set; }
 
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         // Admin / Employee
-        public string Role { get; set; }
+        public required string Role { get; set; }
     }
 }
--- obj/Debug/net9.0/DUANCHAMCONG.AssemblyInfo.cs
+++ obj/Debug/net9.0/DUANCHAMCONG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DUANCHAMCONG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+375d177719a0b18fbc357958bee11571f7407143")]
 [assembly: System.Reflection.AssemblyProductAttribute("DUANCHAMCONG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DUANCHAMCONG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
